872b095d35e2c47abdc5e5eea17c2389dc997f06
LeeXu <hgtlzycwk@gmail.com>
2022-08-05 10:56:00 -0400

    update spacing

--- ColorMaster/ColorMaster/Model/Color.swift
+++ ColorMaster/ColorMaster/Model/Color.swift
@@ -13,7 +13,7 @@ struct Color: Equatable {
     let green: CGFloat
     let blue: CGFloat
     
-    var normalizedColor: (r:CGFloat, g: CGFloat, b: CGFloat) {
+    var normalizedColor: (r: CGFloat, g: CGFloat, b: CGFloat) {
         return (r: red/255.0, g: green/255.0, b: blue/255.0)
     }
 }
